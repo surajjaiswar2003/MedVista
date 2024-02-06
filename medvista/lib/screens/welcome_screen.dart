b2db6e6c5cdae2e815484cbf3e177d45e74305a6
surajjaiswar2003 <sj98998@gmail.com>
2024-02-07 01:11:34 +0530

    Welcome Screen

--- medvista/lib/screens/welcome_screen.dart
+++ medvista/lib/screens/welcome_screen.dart
@@ -16,15 +16,87 @@ class WelcomeScreen extends StatelessWidget {
                 Align(
                   alignment: Alignment.centerRight,
                   child: TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      // Navigator.push(context, MaterialPageRoute(builder:  (context) => ))
+                    },
                     child: const Text(
                       "SKIP",
                       style: TextStyle(
+                        color: Color(0xFF6EB574),
                         fontSize: 20,
                       ),
                     ),
                   ),
-                )
+                ),
+                SizedBox(height: 50),
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Image.asset("assets/images/photo1.png"),
+                ),
+                SizedBox(height: 50),
+                Text(
+                  "Doctor Appointment",
+                  style: TextStyle(
+                    color: Color(0xFF6EB574),
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1,
+                    wordSpacing: 2,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Appoint your Doctor",
+                  style: TextStyle(
+                    color: Color(0xFFb7dfb9),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(height: 60),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Material(
+                      color: Color(0xFF6EB574),
+                      borderRadius: BorderRadius.circular(10),
+                      child: InkWell(
+                        onTap: () {
+                          //Navigator.push(context, MaterialPageRoute(builder:  (context) => ));
+                        },
+                        child: Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 15, horizontal: 40),
+                            child: Text(
+                              "Log In",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                      ),
+                    ),
+                    Material(
+                      color: Color(0xFF6EB574),
+                      borderRadius: BorderRadius.circular(10),
+                      child: InkWell(
+                        onTap: () {
+                          //Navigator.push(context, MaterialPageRoute(builder:  (context) => ));
+                        },
+                        child: Padding(
+                            padding: EdgeInsets.symmetric(
+                                vertical: 15, horizontal: 40),
+                            child: Text(
+                              "Sign Up",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                      ),
+                    ),
+                  ],
+                ),
               ],
             )));
   }
